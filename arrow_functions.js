b70cf504c32a379bc2937ac313a7ea2a9481478e
Sourin <98945276+heysourin@users.noreply.github.com>
2022-06-16 15:27:13 +0530

    Arrow function All Types

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,5 +1,13 @@
 
-// Type 1
+// Type 1: without assigning inside a variable: Arrow function declaration
+clacuAge =(birthYr)=>{
+    return 2022-birthYr;
+}
+
+console.log(clacuAge(1998));
+
+
+//Type2 : Assigning inside a variable: Arrow function expression
 const calcAge=(birthYear)=>{
     return 2022-birthYear;
 }
@@ -7,7 +15,8 @@ const calcAge=(birthYear)=>{
 console.log(calcAge(1998));
 
 
-// Type 2: the parameter is passed without brackets
+
+// Type 3: the parameter is passed without brackets,
 const calcAg1=birthYear1=>{
     return 2022-birthYear1;
 }
@@ -16,7 +25,7 @@ console.log(calcAg1(1999));
 
 
 
-// Type 3: parameter without parenthesis and function without return statement(as it is oneliner)
+// Type 4: parameter without parenthesis and function without return statement(as it is oneliner)
 const calcAge2=birthYear2=> 2022-birthYear2;
 
 console.log(calcAge2(2000));
